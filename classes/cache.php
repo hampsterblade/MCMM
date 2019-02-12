55db7a1b51c9f7cfe33314fec1d26134adb70590
hampsterblade <hampsterblade@gmail.com>
2019-02-12 13:24:21 -0500

    Create blank functions for cache class.

---
+++ classes/cache.php
@@ -0,0 +1,16 @@
+<?php
+class cache
+{
+  function load_page($url)
+  {
+    //checks if page is in cache.  If not cache it and load.
+  }
+  function clean_up()
+  {
+    //checks for cache files more than 24 hours old, and removes them.
+  }
+  function delete(id)
+  {
+    //removes cache file.
+  }
+} ?>
