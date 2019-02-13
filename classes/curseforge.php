8a09efc400d0e88088449c843e2e4db3dd3161e2
hampsterblade <hampsterblade@gmail.com>
2019-02-13 12:27:32 -0500

    Create blank functions for sources and source classes.

---
+++ classes/curseforge.php
@@ -0,0 +1,17 @@
+<?php
+class source
+{
+  function format_url($url)
+  {
+    //takes user entered URL and converts it to a standard format.
+  }
+  function get_num_releases($id)
+  {
+    //gets the total number of versions, and number of pages
+  }
+  function get_info($id, $page_num, $initialize=false)
+  {
+    //Scrapes data to database
+  }
+}
+?>
---
+++ classes/sources.php
@@ -0,0 +1,17 @@
+<?php
+class sources
+{
+  function format_url($url)
+  {
+    //Sends the format_url function to the source function
+  }
+  function get_num_releases($id)
+  {
+    //Sends the get_num_releases function to the source function
+  }
+  function get_info($id, $page_num, $initialize=false)
+  {
+    //Sends the get_info function to the source function
+  }
+}
+?>
