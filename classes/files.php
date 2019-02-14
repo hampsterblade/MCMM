05f2a6b7419ad1feb0ba5d9274781d246879de3e
hampsterblade <hampsterblade@gmail.com>
2019-02-13 23:19:21 -0500

    create blank files functions.

---
+++ classes/files.php
@@ -0,0 +1,21 @@
+<?php
+  class files
+  {
+    function get_file_list($parent)
+    {
+
+    }
+    function download($url, $parent)
+    {
+
+    }
+    function upload($filename, $parent)
+    {
+
+    }
+    function get_file_path($id)
+    {
+      
+    }
+  }
+?>
